13830def01abda4350f2df39110864700dc24080
Raj-nandan <nandanr532@gmail.com>
2024-09-07 10:06:30 +0530

    made changes in src

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <p>hello world</p>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path= "/" element = {<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
